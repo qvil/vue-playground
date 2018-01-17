8599811fa4541a39b678105320ecbfc2e61d239f
Taesu Hyeon <tshyeon@seowonintech.co.kr>
2018-01-17 15:59:26 +0900

    Example for v-if

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ var app = new Vue({
         message: 'Hello Vue!',
     }
 })
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
